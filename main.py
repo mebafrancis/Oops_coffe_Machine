7074452b8f6845b065471e153adf5176a918f182
mebafrancis <melbafrancis@gmail.com>
2025-09-24 18:15:35 +0530

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from menu import Menu
-from coffee_maker import CoffeeMaker
+
 from money_machine import MoneyMachine
+my_money_machine = MoneyMachine()
+
+from coffee_maker import CoffeeMaker
+my_coffee_maker = CoffeeMaker()
+
+from menu import Menu, MenuItem
+my_menu = Menu()
+
+my_coffee_maker.report()
+my_money_machine.report()
