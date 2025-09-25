b86093948ed1ecbeca06f1fc70a9c6bbd9cf77c1
mebafrancis <melbafrancis@gmail.com>
2025-09-25 10:04:21 +0530

    first commit

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ my_menu = Menu()
 
 my_coffee_maker.report()
 my_money_machine.report()
+
+is_on = True
+
+while is_on:
+    options = my_menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        my_coffee_maker.report()
+        my_money_machine.report()
+    else:
+        drink = my_menu.find_drink(choice)
+        if my_coffee_maker.is_resource_sufficient(drink):
+            if my_money_machine.make_payment(drink.cost):
+                my_coffee_maker.make_coffee(drink)
